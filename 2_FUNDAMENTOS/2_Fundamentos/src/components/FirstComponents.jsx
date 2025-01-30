c7831f6d6075192d54a4a40a63a965bbb82548ed
ThiagoSantos7 <seu-email>
2025-01-30 14:47:59 -0300

    adicionando mais components filhos

--- 2_FUNDAMENTOS/2_Fundamentos/src/components/FirstComponents.jsx
+++ 2_FUNDAMENTOS/2_Fundamentos/src/components/FirstComponents.jsx
@@ -1,5 +1,7 @@
 // arquivo de estilo
 
+import MyComponent from "./MyComponent";
+
 const FirstComponents = () => {
   // esta função faz isso
 
@@ -7,6 +9,7 @@ const FirstComponents = () => {
     <div>
       <h2>Meu primeiro componente</h2>
       <p className="teste">Meu texto</p>
+      <MyComponent />
     </div>
   );
 };
