92f6f6cee971c6236650df7cea36e1103fcb0021
ThiagoSantos7 <seu-email>
2025-01-29 22:08:33 -0300

    arrumando projetos react

---
+++ 2_FUNDAMENTOS/2_Fundamentos/src/App.jsx
@@ -0,0 +1,18 @@
+// Components
+import FirstComponents from "./components/FirstComponents";
+
+//styles / css
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Fundamentos React</h1>
+        <FirstComponents />
+      </div>
+    </>
+  );
+}
+
+export default App;
