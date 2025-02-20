49e0350dc2cbeab51c32639c6589c917e766af28
ThiagoSantos7 <seu-email>
2025-02-20 13:20:43 -0300

    aprendendo más praticas de css em react

--- 4_CSS_REACT/React_CSS/src/App.jsx
+++ 4_CSS_REACT/React_CSS/src/App.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import "./App.css";
 import MyComponent from "./components/MyComponent";
 
 function App() {
+  const n = 15;
+  const [name] = useState("Thiago");
   return (
     <>
       {/* CSS Golbal */}
@@ -19,6 +22,20 @@ function App() {
       >
         este elemento foi estilizado em forma inline
       </p>
+      {/* CSS inline dinâmico */}
+      <h2 style={n < 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2 style={n > 10 ? { color: "purple" } : { color: "pink" }}>
+        CSS dinâmico
+      </h2>
+      <h2
+        style={
+          name === "Thiago" ? { color: "green", backgroundColor: "#000" } : null
+        }
+      >
+        Teste Nome
+      </h2>
     </>
   );
 }
